1d36cf1b2890bc51cbaff8cf6ad74aaea831704b
Abraham Galindo <abraham.garo@gmail.com>
2021-10-20 12:36:13 -0500

    Split screen finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from './SplitScreen';
 
-function App() {
+const LeftHandComponent = ({ name }) => {
+  return <h1>{name}!</h1>
+};
+
+const RightHandComponent = ({ name }) => {
+  return <p>{name}!</p>
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitScreen
+      leftWeight={1}
+      rightWeight={3}
+    >
+      <LeftHandComponent name="Hello"/>
+      <RightHandComponent name="My friend"/>
+    </SplitScreen>
   );
 }
 
